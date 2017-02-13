eb85db205154b9f406c7e82507c31519311a756c
Meng Guo <mattguo2015@gmail.com>
2017-02-13 13:23:14 -0500

    font

--- public/js/ctrl/apps.js
+++ public/js/ctrl/apps.js
@@ -20,4 +20,14 @@
                 });
             $locationProvider.html5Mode(true);
         })
+        .directive('moveShadow', function () {
+            return {
+                restrict: 'A', //attribute only
+                link: function(scope, elem, attr, ctrl) {
+                    elem.bind('dblclick', function(e) {
+                        
+                    });
+                }
+            };
+        })
 })();
